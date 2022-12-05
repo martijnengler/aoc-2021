3ed4eeb3a2d3a27cfedd062309c532d6d2b567ca
Martijn Engler <mail@martijnengler.nl>
2022-12-05 16:51:33 +0100

    Build the mapping

--- day-14/day-14-1.php
+++ day-14/day-14-1.php
@@ -3,3 +3,13 @@
 // https://adventofcode.com/2021/day/14
 define("TEST_MODE", true);
 require_once __DIR__ . '/../init.php';
+$template = array_shift($lines);
+array_shift($lines);
+
+$mapping = [];
+foreach($lines as $line)
+{
+	list($a,$b) = explode(" -> ", $line);
+	$mapping[$a] = $b;
+}
+print_r($mapping);
