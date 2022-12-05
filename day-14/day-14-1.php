7a48253467247780a062f0d48e1be5dbec72bd11
Martijn Engler <mail@martijnengler.nl>
2022-12-05 17:07:20 +0100

    Works for one step

--- day-14/day-14-1.php
+++ day-14/day-14-1.php
@@ -1,5 +1,13 @@
 #!/Users/martijnengler/.bin/php
 <?php
+
+/*
+$a = [1,2,3];
+array_splice($a, 1, 0, '4');
+print_r($a);
+exit;
+ */
+
 // https://adventofcode.com/2021/day/14
 define("TEST_MODE", true);
 require_once __DIR__ . '/../init.php';
@@ -13,3 +21,29 @@ foreach($lines as $line)
 	$mapping[$a] = $b;
 }
 print_r($mapping);
+
+$do_steps = 1 ;
+for($i = 0; $i < $do_steps; $i++)
+{
+	$geefditeennaam = 0;
+	$splices_to_do = [];
+	$chars = str_split($template);
+	foreach($chars as $key => $val)
+	{
+		if(!isset($chars[$key+1]))
+			break;
+
+		$str = $val . $chars[$key+1];
+		if(isset($mapping[$str]))
+		{
+			$geefditeennaam++;
+			$splices_to_do[$key + $geefditeennaam] = $mapping[$str];
+		}
+	}
+
+	foreach($splices_to_do as $offset => $val)
+	{
+		array_splice($chars, $offset, 0, $val);
+	}
+	print_r(implode("", $chars));
+}
