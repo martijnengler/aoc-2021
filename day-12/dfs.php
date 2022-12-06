1688e1450c812d1584b5122d24cfd4c0ad208b04
Martijn Engler <mail@martijnengler.nl>
2022-12-06 19:11:26 +0100

    Do some formatting

--- day-12/dfs.php
+++ day-12/dfs.php
@@ -11,23 +11,42 @@ class Node
 
 	public function link_to(Node $node, $also = true)
 	{
-		if (!$this->linked($node)) $this->linked[] = $node;
-		if ($also) $node->link_to($this, false);
+		if (!$this->linked($node))
+		{
+			$this->linked[] = $node;
+		}
+
+		if ($also)
+		{
+			$node->link_to($this, false);
+		}
+
 		return $this;
 	}
 
 	private function linked(Node $node)
 	{
-		foreach ($this->linked as $l) { if ($l->name === $node->name) return true; }
+		foreach ($this->linked as $l)
+		{
+			if($l->name === $node->name)
+			{
+				return true;
+			}
+		}
 		return false;
 	}
 
 	public function not_visited_nodes($visited_names)
 	{
 		$ret = array();
-		foreach ($this->linked as $l) {
-			if (!in_array($l->name, $visited_names)) $ret[] = $l;
+		foreach ($this->linked as $l)
+		{
+			if(!in_array($l->name, $visited_names))
+			{
+				$ret[] = $l;
+			}
 		}
+
 		return $ret;
 	}
 }
@@ -53,13 +72,20 @@ $output = [];
 function dfs(Node $node, $path = '', $visited = array())
 {
 	global $output;
+
 	$visited[] = $node->name;
 	$not_visited = $node->not_visited_nodes($visited);
-	if (empty($not_visited)) {
+
+	if(empty($not_visited))
+	{
 		$output[] = ltrim($path . ',' . $node->name . PHP_EOL, ',');
 		return;
 	}
-	foreach ($not_visited as $n) dfs($n, $path . ',' . $node->name, $visited);
+
+	foreach($not_visited as $n)
+	{
+	 	dfs($n, $path . ',' . $node->name, $visited);
+	}
 }
 
 dfs($start);
