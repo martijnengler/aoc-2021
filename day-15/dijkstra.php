4a2eb4b6cfd6fc69918c7a52c21b18ad634cdafc
Martijn Engler <mail@martijnengler.nl>
2022-12-08 19:34:18 +0100

    Remove the commented code part 1

--- day-15/dijkstra.php
+++ day-15/dijkstra.php
@@ -53,20 +53,6 @@ foreach($reached as $key => $x)
 }
 
 /*
-frontier = Queue()
-frontier.put(start )
-reached = set()
-reached.add(start)
-
-while not frontier.empty():
-	current = frontier.get()
-	for next in graph.neighbors(current):
-		if next not in reached:
-			frontier.put(next)
-			reached.add(next)
-*/
-
-/*
 frontier = PriorityQueue()
 frontier.put(start, 0)
 came_from = dict()
