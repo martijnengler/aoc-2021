c20ad44243eb91cf54618ef03843877ffc766bef
Martijn Engler <mail@martijnengler.nl>
2022-12-14 23:26:47 +0100

    Create a nice common.php

---
+++ day-15/common.php
@@ -0,0 +1,54 @@
+<?php
+ini_set('memory_limit', '4096M');
+
+// https://adventofcode.com/2021/day/15
+define("TEST_MODE",       true);
+define('AOC_INPUT_FILE',  "");
+define('SPLIT_CHARACTER', '');
+
+require_once __DIR__ . '/../init.php';
+use MartijnEngler\Pathfinding\Node as Node;
+use MartijnEngler\Pathfinding\GridWithWeights as GridWithWeights;
+
+function copyRow($row)
+{
+	return array_map(function($x){
+		$range = range(1,9);
+		return wrapAround($range, $step = 1, $index = $x - 1);
+	}, $row);
+}
+
+foreach($lines as &$line)
+{
+	$copies = [];
+	$copy_line = $line;
+
+	for($i = 0; $i < TIMES_LARGER - 1; $i++)
+	{
+		$copy_line = copyRow($copy_line);
+		$copies[] = $copy_line;
+	}
+
+	$line =  array_merge($line, arrayFlatten($copies));
+}
+
+$original_count = count($lines);
+
+reset($lines);
+for($i = 0; $i < TIMES_LARGER - 1; $i++)
+{
+	// do not call $something $line because of the by ref above
+	foreach(array_slice($lines, $i * $original_count, $original_count) as $something)
+	{
+		$copy_line = copyRow($something);
+		$lines[] = $copy_line;
+	}
+}
+
+$grid = buildGridFromArray($lines);
+
+$start = new Node(0,0);
+$goal  = new Node(count($grid->grid) - 1, count($grid->grid) - 1);
+
+list($came_from, $cost_so_far) = dijkstra_search($grid, $start, $goal);
+printf("%d\n", array_pop($cost_so_far));
--- day-15/day-15-1.php
+++ day-15/day-15-1.php
@@ -1,18 +1,4 @@
 #!/Users/martijnengler/.bin/php
 <?php
-// https://adventofcode.com/2021/day/15
-define("TEST_MODE",       true);
-define('AOC_INPUT_FILE',  "");
-define('SPLIT_CHARACTER', '');
-
-require_once __DIR__ . '/../init.php';
-use MartijnEngler\Pathfinding\Node as Node;
-use MartijnEngler\Pathfinding\GridWithWeights as GridWithWeights;
-
-$grid = buildGridFromArray($lines);
-
-$start = new Node(0,0);
-$goal  = new Node(count($grid->grid) - 1, count($grid->grid) - 1);
-
-list($came_from, $cost_so_far) = dijkstra_search($grid, $start, $goal);
-printf("%d\n", array_pop($cost_so_far));
+define("TIMES_LARGER", 0);
+require_once 'common.php';
