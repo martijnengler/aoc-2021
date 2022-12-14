c60bb0efbacf9a64f0480dd3b35912c943380d82
Martijn Engler <mail@martijnengler.nl>
2022-12-14 22:06:40 +0100

    Well, 2021-15 is done

--- day-15/day-15-1.php
+++ day-15/day-15-1.php
@@ -3,18 +3,24 @@
 // https://adventofcode.com/2021/day/15
 define("TEST_MODE",       true);
 define('AOC_INPUT_FILE',  "");
-define('SPLIT_CHARACTER', null);
+define('SPLIT_CHARACTER', '');
 
 require_once __DIR__ . '/../init.php';
+use MartijnEngler\Pathfinding\Node as Node;
+use MartijnEngler\Pathfinding\GridWithWeights as GridWithWeights;
 
-array_map(fn($x) => $x, $lines);
-array_map(function($x){
-	return $x;
-}, $lines);
+$grid = buildGridFromArray($lines);
 
-print_r($lines);
+$start = new Node(0,0);
+$goal  = new Node(count($grid->grid) - 1, count($grid->grid) - 1);
 
-foreach($lines as $line)
-{
+list($came_from, $cost_so_far) = dijkstra_search($grid, $start, $goal);
+printf("%d\n", array_pop($cost_so_far));
+exit;
 
-}
+draw_grid(
+	$grid,
+	[
+		'path' => reconstruct_path($came_from, $start, $goal)
+	]
+);
