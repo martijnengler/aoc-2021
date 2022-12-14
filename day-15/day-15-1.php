d6080465bcef9416adce58cf771c96ba887c2e7a
Martijn Engler <mail@martijnengler.nl>
2022-12-14 21:57:05 +0100

    Initial commit

---
+++ day-15/day-15-1.php
@@ -0,0 +1,20 @@
+#!/Users/martijnengler/.bin/php
+<?php
+// https://adventofcode.com/2021/day/15
+define("TEST_MODE",       true);
+define('AOC_INPUT_FILE',  "");
+define('SPLIT_CHARACTER', null);
+
+require_once __DIR__ . '/../init.php';
+
+array_map(fn($x) => $x, $lines);
+array_map(function($x){
+	return $x;
+}, $lines);
+
+print_r($lines);
+
+foreach($lines as $line)
+{
+
+}
