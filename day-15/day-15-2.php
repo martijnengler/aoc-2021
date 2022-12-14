7d4aa0e99c387a161715df60edbba61009b55f81
Martijn Engler <mail@martijnengler.nl>
2022-12-14 22:22:39 +0100

    Copying works, at least for first row

--- day-15/day-15-2.php
+++ day-15/day-15-2.php
@@ -9,14 +9,42 @@ require_once __DIR__ . '/../init.php';
 use MartijnEngler\Pathfinding\Node as Node;
 use MartijnEngler\Pathfinding\GridWithWeights as GridWithWeights;
 
+function copyRow($row)
+{
+	return array_map(function($x){
+		$range = range(1,9);
+		return wrapAround($range, $step = 1, $index = $x - 1);
+	}, $row);
+}
+
+foreach($lines as &$line)
+{
+	$copies = [];
+	$copy_line = $line;
+
+	$copy_line = copyRow($copy_line);
+	$copies[] = $copy_line;
+	$copy_line = copyRow($copy_line);
+	$copies[] = $copy_line;
+	$copy_line = copyRow($copy_line);
+	$copies[] = $copy_line;
+	$copy_line = copyRow($copy_line);
+	$copies[] = $copy_line;
+
+	$line =  array_merge($line, arrayFlatten($copies));
+}
+
+printf("xxx%s\n", implode("", $lines[0]));
+exit;
+showMatrix($lines, '');
+exit;
+
 $grid = buildGridFromArray($lines);
 
 $start = new Node(0,0);
 $goal  = new Node(count($grid->grid) - 1, count($grid->grid) - 1);
 
 list($came_from, $cost_so_far) = dijkstra_search($grid, $start, $goal);
-printf("%d\n", array_pop($cost_so_far));
-exit;
 
 draw_grid(
 	$grid,
