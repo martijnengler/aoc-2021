6466636321673650f4c511349e3c252c740a6ffb
Martijn Engler <mail@martijnengler.nl>
2022-12-14 21:52:40 +0100

    Copy init and skeleton from 2022

--- init.php
+++ init.php
@@ -1,12 +1,29 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/../lib/php/libinit.php';
-$input_filename = dirname($argv[0]) . DIRECTORY_SEPARATOR . '/';
-if(defined('INPUT_FILE'))
+
+if(isset($argv[1]) && strlen($argv[1]) > 0)
 {
-	$input_filename .= INPUT_FILE;
+	$input_filename = $argv[1];
+}
+elseif(defined("AOC_INPUT_FILE") && strlen(AOC_INPUT_FILE) > 0)
+{
+	$input_filename = AOC_INPUT_FILE;
 }
 else
 {
+	$input_filename = dirname($argv[0]) . DIRECTORY_SEPARATOR . '/';
 	$input_filename .= (defined('TEST_MODE') && TEST_MODE) ? 'test' : 'input';
 }
 $lines = file($input_filename, FILE_IGNORE_NEW_LINES);
+$original_lines = $lines;
+
+if(defined("SPLIT_CHARACTER") && !is_null(SPLIT_CHARACTER))
+{
+	$lines = splitArrayValues(SPLIT_CHARACTER, $lines);
+
+	// set this for legacy reasons:
+	$split_lines = $lines;
+}
+
+$climate = new League\CLImate\CLImate;
--- skeleton.php
+++ skeleton.php
@@ -1,5 +1,20 @@
 #!/Users/martijnengler/.bin/php
 <?php
 // https://adventofcode.com/XXXYEARXXX/day/XXXDAYXXX
-define("TEST_MODE", true);
+define("TEST_MODE",       true);
+define('AOC_INPUT_FILE',  "");
+define('SPLIT_CHARACTER', null);
+
 require_once __DIR__ . '/../init.php';
+
+array_map(fn($x) => $x, $lines);
+array_map(function($x){
+	return $x;
+}, $lines);
+
+print_r($lines);
+
+foreach($lines as $line)
+{
+
+}
