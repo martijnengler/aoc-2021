5fe445827e145e299a726a5a2d68be7a5989359e
Martijn Engler <mail@martijnengler.nl>
2023-12-24 11:24:49 +0100

    Test parsing works

--- day-13/day-13-1.php
+++ day-13/day-13-1.php
@@ -3,18 +3,35 @@
 // https://adventofcode.com/2021/day/13
 define("TEST_MODE",       true);
 define('AOC_INPUT_FILE',  "");
-define('SPLIT_CHARACTER', null);
+define('SPLIT_CHARACTER', ',');
 
 require_once __DIR__ . '/../init.php';
 
-$lines = array_map(fn($x) => $x, $lines);
-$lines = array_map(function($x){
-	return $x;
-}, $lines);
+$folds_part = false;
+$dots       = [];
+$folds      = [];
 
-print_r($lines);
-
-foreach($lines as $line)
+foreach($lines as &$line)
 {
+	if($line[0] === '')
+	{
+		$folds_part = true;
+		continue;
+	}
 
+	if(!$folds_part)
+	{
+		$dots[] = ['row' => $line[0], 'col' => $line[1]];
+	}
+
+	else
+	{
+		$parts = explode('=', $line[0]);
+		$axis  = substr($parts[0], -1);
+		$value = $parts[1];
+		$folds[] = ['axis' => $axis, 'value' => $value];
+	}
 }
+
+print_r($dots);
+print_r($folds);
